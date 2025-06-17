2a02d19c105383c7816ae1a63c8ee403a4ea5ef4
Rosa <costurezati@gmail.com>
2025-06-17 09:56:28 -0300

    .

--- app/includes/header.php
+++ app/includes/header.php
@@ -7,10 +7,7 @@
     <link rel="stylesheet" href="./ressources/css/style.css">
     <link rel="stylesheet" href="./ressources/css/header.css">
     <link rel="stylesheet" href="./ressources/css/media.css">
-    <link rel="stylesheet" href="./ressources/css/footer.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Instagrama</title>
 </head>
 
@@ -27,10 +24,6 @@
         </div>
 
     </header>
-
-    <footer>
-        <img src="./ressources/img/costureza.png" alt="">
-    </footer>
 
 
     <script src="./ressources/js/script.js"></script>
